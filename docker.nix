f26ac8b6d703feab981f2422c1f18165f3523d30
Martin Betak <matobet@gmail.com>
2021-01-24 18:15:58 +0100

    Fix Ctrl-C in container

--- docker.nix
+++ docker.nix
@@ -1,11 +1,23 @@
 { pkgs ? import <nixpkgs> {}, dyco-mirror-bot ? import ./default.nix {} }:
 
-let dyco-mirror-bot-exe = dyco-mirror-bot.projectCross.raspberryPi.hsPkgs.dyco-mirror-bot.components.exes.dyco-mirror-bot-exe;
+let
+  dyco-mirror-bot-exe = dyco-mirror-bot.projectCross.raspberryPi.hsPkgs.dyco-mirror-bot.components.exes.dyco-mirror-bot-exe;
+
+  entrypoint = pkgs.writeScript "entrypoint.sh" ''
+  #!${pkgs.stdenv.shell}
+  $@
+  '';
 in
 pkgs.dockerTools.buildImage {
   name = "dyco-mirror-bot";
-  contents = [ dyco-mirror-bot-exe pkgs.iana-etc pkgs.cacert ];
+  contents = [ pkgs.bashInteractive pkgs.coreutils dyco-mirror-bot-exe pkgs.iana-etc pkgs.cacert ];
+  # runAsRoot = ''
+  #   #!${pkgs.stdenv.shell}
+  #   ${pkgs.dockerTools.shadowSetup}
+  #   rm -rf /nix/**/*gcc-{,debug-}9*
+  # '';
   config = {
     Cmd = [ "${dyco-mirror-bot-exe}/bin/dyco-mirror-bot-exe" ];
+    Entrypoint = [ entrypoint ];
   };
 }
